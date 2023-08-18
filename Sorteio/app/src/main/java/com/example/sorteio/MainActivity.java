d182553a9dd587a782f8c53ef01a24f3e48ddb93
maria <mariaeduardamaffezzolli@gmail.com>
2023-08-18 19:18:26 -0300

    arrumando

--- Sorteio/app/src/main/java/com/example/sorteio/MainActivity.java
+++ Sorteio/app/src/main/java/com/example/sorteio/MainActivity.java
@@ -10,38 +10,29 @@ import android.widget.TextView;
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
-    EditText edMin,edMax;
-
-    TextView tvReultado1,  tvReultado2;
+    EditText edMin, edMax;
+    TextView tvResultado1, tvResultado2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
         edMin = findViewById(R.id.edMin);
         edMax = findViewById(R.id.edMax);
-        tvReultado1=findViewById(R.id.txt1);
-        tvReultado2=findViewById(R.id.txt2);
-
+        tvResultado1 = findViewById(R.id.txt1);
+        tvResultado2 = findViewById(R.id.txt2);
     }
 
+    public void sorteia(View v) {
+        int min = Integer.parseInt(edMin.getText().toString());
+        int max = Integer.parseInt(edMax.getText().toString());
 
-    public void sorteia(View v){
-        int min,max;
-
-        min = Integer.parseInt(edMin.getText().toString());
-        max = Integer.parseInt(edMax.getText().toString());
-
-        int random =(int) (Math.random()* (max=min+1))+min;
-        tvReultado1.setText(Integer.toString(random));
+        int random1 = (int) (Math.random() * (max - min + 1)) + min;
+        tvResultado1.setText(Integer.toString(random1));
 
         Random utilRandom = new Random();
-        random = utilRandom.nextInt(max-min+1)+min;
-        tvReultado2.setText(Integer.toString(random));
+        int random2 = utilRandom.nextInt(max - min + 1) + min;
+        tvResultado2.setText(Integer.toString(random2));
     }
-
-
-
-    }
+}
